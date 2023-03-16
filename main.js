9aa0490767f90cfc3849eaa907119f4ceaab42a7
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2023-03-16 22:01:23 +0700

    baseProject

---
+++ main.js
@@ -0,0 +1,12 @@
+// let tahunMerdeka = 1945;
+// let tahunLamaMerdeka = 2023;
+// console.log(tahunLamaMerdeka - tahunMerdeka);
+
+// let score = 1;
+// console.log(score)
+
+// const merdeka = 1945;
+
+// let ayam = true;
+// const isLogin = false;
+// console.log(isLogin);
