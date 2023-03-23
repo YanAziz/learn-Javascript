c7a0ad1b1af914794c442195e4a4aadb0146164f
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2023-03-23 10:53:47 +0700

    javascriptLogic

--- main.js
+++ main.js
@@ -3,10 +3,89 @@
 // console.log(tahunLamaMerdeka - tahunMerdeka);
 
 // let score = 1;
-// console.log(score)
+// console.log(score);
 
 // const merdeka = 1945;
 
 // let ayam = true;
 // const isLogin = false;
 // console.log(isLogin);
+
+// let a = 'I can not "sleep"';
+// console.log(a);
+
+// const nilaiUjian = 90;
+
+// if (nilaiUjian < 50) {
+//   console.log("E");
+// } else if (nilaiUjian < 60) {
+//   console.log("D");
+// } else if (nilaiUjian < 70) {
+//   console.log("C");
+// } else if (nilaiUjian < 80) {
+//   console.log("B");
+// } else if (nilaiUjian < 100) {
+//   console.log("A");
+// }
+
+// const thisDay = prompt("Isikan hari!").toLowerCase();
+
+// if (thisDay === "senin") {
+//   console.log("Kerja woy!!");
+// } else if (thisDay === "sabtu") {
+//   console.log("Yeayy libur");
+// } else {
+//   console.log("Biasa aja");
+// }
+
+// const password = prompt("Isikan Password!");
+
+// if (password.length >= 6) {
+//   if (password.indexOf(" ") === -1) {
+//     console.log("Password valid");
+//   } else {
+//     console.log("Password tidak boleh ada spasi!");
+//   }
+// } else {
+//   console.log("Password minimal 6 karakter");
+// }
+
+// const gender = "male";
+// const age = 11;
+
+// if (gender === "male" && age >= 19) {
+//   console.log("Legal!");
+// } else {
+//   console.log("Masih bocil");
+// }
+
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//   console.log("Password Valid");
+// } else {
+//   console.log("Buat Ulang!");
+// }
+
+// const role = prompt("Isikan Role!").toLowerCase();
+
+// if (role === "admin" || role === "spv") {
+//   console.log("Akses Penuh");
+// } else {
+//   console.log("Akses Ditolak");
+// }
+
+// if (role !== "admin") {
+//   console.log("Akses Ditolak");
+// } else {
+//   console.log("Akses Penuh");
+// }
+
+// const balonku = prompt("Isikan warna").toLowerCase();
+// switch (balonku) {
+//   case "merah":
+//     console.log("warna merah");
+//     break;
+//   case "hijau":
+//     console.log("Doorrrr!!!");
+//   default:
+//     console.log("Bukan balon saya");
+// }
