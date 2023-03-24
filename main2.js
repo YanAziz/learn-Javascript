1cd82d93ae39bc57bf8019421183b9ba7229deed
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2023-03-24 16:31:15 +0700

    learnObject

---
+++ main2.js
@@ -0,0 +1,12 @@
+// // Struktur data objek
+// const fitnessData = {
+//     totalSteps : 1000,
+//     totalMiles : 120,
+//     avgCaloriesBurn : 150,
+// }
+
+// // fitnessData --> Object name
+// // totalSteps, total Miles, avgCaloriesBurn --> Object Property, key
+// // 1000, 120, 150 --> value
+
+// console.log(fitnessData);
