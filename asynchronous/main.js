fec0a71eee2b3c6bc72321358c710c022dbb9c77
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2023-06-06 18:50:54 +0700

    first commit

---
+++ asynchronous/main.js
@@ -0,0 +1,45 @@
+// Call Stack -> mekanisme memori di js  -- (rumusPhy)
+
+/* 
+Call Stack Function
+
+yaitu mekanisme untuk interpreter (seperti interpreter Javascript di browser web) untuk
+melacak lokasi skrip yang memanggil banyak fungsi-fungsi yang sedang dijalankan dan
+fungsi apa yang dipanggil dari dalam fungsi itu, dll
+
+*/
+
+// ------------------------------------------------------------------
+
+// Faktanya Javascript itu Single Thread
+
+/* 
+Aslinya Javascript dibaca secara sinkronus -- (example)
+
+Single thread adalah sebuah program yang hanya dapat menjalankan satu proses dalam satu waktu
+
+Cara Kerjanya
+- Browser dibuat dengan API yang bisa menjalankan tugas dibalik layar (kaya bikin setTimeOut)
+- Javascript Call Stack memahami fungsi-fungsi API sehingga bisa memanfaatkan kerjaan browser
+- Pada saat browser selesai menjalankan tugas, hasilnya akan dimasukkan ke dalam stack 
+    sebagai callback
+
+*/
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
