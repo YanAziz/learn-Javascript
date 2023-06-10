e1bea1bca15b22189313ff397aba836f095e27da
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2023-06-10 20:35:49 +0700

    new-AJAX-WebAPI

---
+++ AJAX-WebAPI/main.js
@@ -0,0 +1,46 @@
+// Pengenalan AJAX Request
+
+/* 
+XMLHTTP - FETCH - AXIOS 
+
+AJAX --> Asynchronous JavaScript and XML
+
+JSON
+
+Search secara Real-time
+*/
+
+// ------------------------------------------------------------------------------------------------
+
+// Mengenal API
+
+/* 
+API - Application Programming Interface
+
+Web API - JSON 
+*/
+
+// ------------------------------------------------------------------------------------------------
+
+// Apa itu JSON
+
+/*
+XML --> cari google
+
+AJAJ --> JSON
+
+JSON --> Java Script Object Notation
+
+
+
+
+*/
+
+
+// ------------------------------------------------------------------------------------------------
+
+// lebih mudah akses web api dengan Postman dan Hoppscotch
+
+// ------------------------------------------------------------------------------------------------
+
+// Mengenal Http Verb atau Http Method
