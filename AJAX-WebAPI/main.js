987ffac9f594f0c3529405c0445750a9e5eddfc1
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2023-06-29 00:01:09 +0700

    QueryString

--- AJAX-WebAPI/main.js
+++ AJAX-WebAPI/main.js
@@ -31,16 +31,85 @@ AJAJ --> JSON
 
 JSON --> Java Script Object Notation
 
+json.org (link references)
 
-
-
+JSON.parse --> akses 
+JSON.stringify --> mengubah format ke JSON
 */
-
 
 // ------------------------------------------------------------------------------------------------
 
 // lebih mudah akses web api dengan Postman dan Hoppscotch
 
+/*
+Postman --> Ngambil data sama spt Hoppscotch tapi perlu login
+
+Cat Fatch API
+*/
+
 // ------------------------------------------------------------------------------------------------
 
 // Mengenal Http Verb atau Http Method
+
+/*
+Web Cat Facts API
+
+Http Verb --> GET, POST, PATCH, DELETE, dll
+*/
+
+// ------------------------------------------------------------------------------------------------
+
+// Penjelasan Kode Angka dari Respon Http
+
+/*
+HTTP Response status codes by MDN
+
+a. Informational Responses --> 100 - 199
+b. Successful Responses --> 200 - 299
+c. Redirection Mesagges --> 300 - 399
+d. Client Error Repponses --> 400 - 499
+e. Server Error Responses --> 500 - 599
+
+*/
+
+// ------------------------------------------------------------------------------------------------
+
+// Mengenal Base Url dan Query String di Web Api
+
+/*
+SWAPI
+
+?sort=desc&price=1000 --> query string
+
+?, key, =, value
+
+Url Incode
+
+TVMAZE
+
+*/
+
+// ------------------------------------------------------------------------------------------------
+
+// Mengenal Data di Http Header
+
+// ------------------------------------------------------------------------------------------------
+
+// Melakukan Request dengan XML http Prequest
+
+// ------------------------------------------------------------------------------------------------
+
+// Melakukan Request dengan Fetch
+
+// ------------------------------------------------------------------------------------------------
+
+// Melakukan Request dengan Library Axios
+
+// ------------------------------------------------------------------------------------------------
+
+// Menambahkan Data Headers di Request menggunakan Axios
+
+// ------------------------------------------------------------------------------------------------
+
+// Menampilkan Daftar Acara TV dengan Axios
+
