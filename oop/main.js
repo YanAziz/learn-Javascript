9119e398e387eb9926f412abfa548e3ff31c6a96
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2023-07-01 21:09:03 +0700

    oop-js

---
+++ oop/main.js
@@ -0,0 +1,32 @@
+// Perkenelan tentang Prototype di Javascript
+
+/*
+Prototype --> Mekanisme suatu Js bisa menurunkan suatu fitur yang dimliki
+oleh suatu objek ke objek lainnya atau objek turunannya
+
+MDN for references -- Array
+
+length, pop, map, dll <-- Prototype 
+
+Intinya prototype
+*/
+
+// ----------------------------------------------------------------------------
+
+// Membuat Object dengan Factory Function
+
+// ----------------------------------------------------------------------------
+
+// Membuat Object dengan Constructor
+
+// ----------------------------------------------------------------------------
+
+// Membuat Object dengan Class
+
+// ----------------------------------------------------------------------------
+
+// Contoh lanjut Object dengan Class
+
+// ----------------------------------------------------------------------------
+
+// Penjelasan OOP, Extend dan Fungsi Super
